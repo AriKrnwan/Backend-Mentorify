5f10de2a98139f4c7a412837473824ba35e1a2ac
AriKrnwan <ari.20057@mhs.unesa.ac.id>
2023-12-10 12:21:15 +0700

    REGISTER DONE

--- src/index.js
+++ src/index.js
@@ -1,18 +1,70 @@
 require('dotenv').config()
 const PORT = process.env.PORT || 5000;
-
+const mysql = require('mysql2');
+const cors = require('cors')
 const express = require('express');
-const usersRoutes = require('./routes/users.js');
 const rolesRoutes = require('./routes/roles.js')
 const app = express();
 const middlewareLogRequest = require('./middleware/logs');
+const db = require ('./config/database.js')
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// REGISTER LOGIN
+app.use(cors())
+
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    const sentName = req.body.name;
+    const sentPassword = req.body.password;
+    const sentEmail = req.body.email;
+
+    if (!sentName || !sentPassword || !sentEmail) {
+        return res.status(400).send({ message: 'Invalid request. Missing required fields.' });
+    }
+
+    const SQL = 'INSERT INTO users (full_name, password, email) SELECT ?, ?, ? FROM DUAL WHERE NOT EXISTS (SELECT * FROM users WHERE email = ?)';
+    const Values = [sentName, sentPassword, sentEmail, sentEmail];
+    db.query(SQL, Values, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ message: 'Internal Server Error' });
+        } else {
+            if (result.affectedRows === 0) {
+                // Jika tidak ada baris yang terpengaruh, artinya email sudah ada
+                res.status(409).send({ message: 'Email already exists' });
+            } else {
+                console.log('User inserted successfully');
+                res.status(200).send({ message: 'User added' });
+            }
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// REGISTER LOGIN
 
 app.use(middlewareLogRequest);
 
 app.use(express.json());
 
-app.use('/users', (usersRoutes))
-app.use('/roles', (rolesRoutes))
+app.use('/roles', rolesRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running di port ${PORT}`)
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-// const { findAllJadwals, getJadwalById, createNewJadwals } = require('../controller/jadwalsControler')
-
-// const router = require('express').Router()
-
-// router.get('/jadwals', findAllJadwals)
-// router.get('/jadwals/:id', getJadwalById)
-// router.post('/jadwals', createNewJadwals)
-
-// module.exports = router
