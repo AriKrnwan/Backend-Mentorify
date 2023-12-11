487bbeda2aa1b48bfea972e671aafb5050282872
sambaspe <sambaspe09@gmail.com>
2023-12-11 20:53:14 +0700

    login/register

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const mysql = require('mysql2');
 const cors = require('cors');
+const bcrypt = require('bcryptjs')
 const express = require('express');
 const rolesRoutes = require('./routes/roles.js');
 const app = express();
@@ -16,59 +17,81 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.post('/register', async (req, res) => {
-    try {
-        const { full_name, password, email } = req.body;
+  try {
+      const { full_name, password, email, institution, phone } = req.body;
 
-        if (!full_name || !password || !email) {
-            return res.status(400).send({ message: 'Invalid request. Missing required fields.' });
-        }
+      // Validasi minimal kolom yang diperlukan
+      if (!full_name || !email || !password) {
+          return res.status(400).send({ message: 'Invalid request. Email and password are required.' });
+      }
 
-        const SQL = 'INSERT INTO users (full_name, password, email) SELECT ?, ?, ? FROM DUAL WHERE NOT EXISTS (SELECT * FROM users WHERE email = ?)';
-        const values = [full_name, password, email, email];
+      // Tambahkan nilai default untuk role_id atau atur nilai yang sesuai dengan skema Anda
+      const role_id = 1;
 
-        const [result] = await db.query(SQL, values);
+      // Hash password menggunakan bcrypt
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-        if (result.affectedRows === 0) {
-            // Jika tidak ada baris yang terpengaruh, artinya email sudah ada
-            return res.status(409).send({ message: 'Email already exists' });
-        }
+      // Membangun array values sesuai dengan kolom yang akan diisi
+      const values = [role_id, full_name, hashedPassword, email, institution, phone];
+      const placeholders = ['role_id', 'full_name', 'password', 'email', 'institution', 'phone'];
 
-        console.log('User inserted successfully');
-        return res.status(200).send({ message: 'User added' });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send({ message: 'Internal Server Error', error: error.message });
-    }
+      const placeholdersStr = placeholders.join(', ');
+      const placeholdersValues = Array(placeholders.length).fill('?').join(', ');
+
+      const SQL = `INSERT INTO users (${placeholdersStr}) 
+                   SELECT ${placeholdersValues} FROM DUAL 
+                   WHERE NOT EXISTS (SELECT * FROM users WHERE email = ?)`;
+
+      const [result] = await db.query(SQL, [...values, email]);
+
+      if (result.affectedRows === 0) {
+          // Jika tidak ada baris yang terpengaruh, artinya email sudah ada
+          return res.status(409).send({ message: 'Email already exists' });
+      }
+
+      console.log('User inserted successfully');
+      return res.status(200).send({ message: 'User added' });
+  } catch (error) {
+      console.error(error);
+      return res.status(500).send({ message: 'Internal Server Error', error: error.message });
+  }
 });
+
 
 // LOGIN
 app.post('/login', async (req, res) => {
-    try {
+  try {
       const { email, password } = req.body;
-  
+
       if (!email || !password) {
-        return res.status(400).json({ message: 'Invalid request. Missing required fields.' });
+          return res.status(400).json({ message: 'Invalid request. Missing required fields.' });
       }
-  
-      const SQL = 'SELECT * FROM users WHERE email = ? AND password = ?';
-      const values = [email, password];
-  
+
+      const SQL = 'SELECT * FROM users WHERE email = ?';
+      const values = [email];
+
       // Ganti 'execute' atau 'query' sesuai dengan metode yang Anda gunakan
       const [results] = await db.execute(SQL, values);
-  
-      if (results.length === 0) {
-        return res.status(401).json({ message: 'Invalid credentials', details: 'Email or password is incorrect.' });
 
+      if (results.length === 0) {
+          return res.status(401).json({ message: 'Invalid credentials', details: 'Email or password is incorrect.' });
       }
-  
+
       const user = results[0];
-      // Sesuaikan respons berdasarkan kebutuhan Anda
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+          return res.status(401).json({ message: 'Invalid credentials', details: 'Email or password is incorrect.' });
+      }
+
+      // Jika password valid, Anda dapat melanjutkan dengan respons login yang berhasil
       return res.status(200).json({ message: 'Login successful', user });
-    } catch (error) {
+
+  } catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Internal Server Error', error: error.message });
-    }
-  });
+  }
+});
   
   
 
