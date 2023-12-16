8c3350faf98e250583625b637744eaf10b5d278f
AriKrnwan <ari.20057@mhs.unesa.ac.id>
2023-12-16 10:32:27 +0700

    P

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 5000;
 const mysql = require('mysql2');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken'); // Add this line for JWT
+// const jwt = require('jsonwebtoken'); // Add this line for JWT
 const express = require('express');
 const rolesRoutes = require('./routes/roles.js');
 const app = express();
@@ -11,11 +11,31 @@ const middlewareLogRequest = require('./middleware/logs');
 const db = require('./config/database.js');
 
 const bodyParser = require('body-parser');
+// const jwtMiddleware = require('./middleware/jwt');
+const expressSession = require('express-session');
+
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET || 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+
 // REGISTER
-app.use(cors());
 
 app.post('/register', async (req, res) => {
   try {
@@ -87,34 +107,106 @@ app.post('/login', async (req, res) => {
       return res.status(401).json({ message: 'Invalid credentials', details: 'Email or password is incorrect.' });
     }
 
-    // Generate JWT token
-    const token = jwt.sign({ email, role_id: user.role_id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    // Simpan informasi pengguna dalam session
+    req.session.user = {
+      id: user.id,
+      email: user.email,
+      role_id: user.role_id,
+      full_name: user.full_name,
+      institution: user.institution, 
+      phone: user.phone,
+      birth_date: user.birth_date,
+      gender: user.gender,
+      topic: user.topic,
+      skill: user.skill,
+      bio: user.bio,
+      rating: user.rating,
+      review: user.review,
+      certification: user.certification,
+      experience: user.experience,
+      city: user.city,
+      time_zone: user.time_zone,
+      balance: user.balance
+    };
+
 
     // Jika password valid, Anda dapat melanjutkan dengan respons login yang berhasil
-    return res.status(200).json({ message: 'Login successful', user, token });
+    return res.status(200).json({ message: 'Login successful', user });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Internal Server Error', error: error.message });
   }
 });
 
-// middleware verifikasi token
-// const verifyToken = (req, res, next) => {
-//     const token = req.headers.authorization;
 
-//     if (!token) {
-//         return res.status(401).json({ message: 'Unauthorized' });
-//     }
+app.get('/profile', (req, res) => {
+  // Periksa apakah pengguna telah login
+  if (req.session.user) {
+    return res.status(200).json({ message: 'User profile retrieved successfully', user: req.session.user });
+  } else {
+    return res.status(401).json({ message: 'User not authenticated' });
+  }
+});
 
-//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//         if (err) {
-//             return res.status(403).json({ message: 'Forbidden', error: err.message });
-//         }
+app.put('/profile', async (req, res) => {
+  try {
+    // Pastikan pengguna telah login
+    if (!req.session.user) {
+      return res.status(401).json({ message: 'User not authenticated' });
+    }
 
-//         req.user = decoded;
-//         next();
-//     });
-// };
+    // Dapatkan ID pengguna dari sesi
+    const userId = req.session.user.id;
+
+    // Dapatkan data yang ingin diupdate dari body permintaan
+    const { full_name, institution, phone, birth_date, gender, topic, skill, bio, certification, experience, city, time_zone } = req.body;
+
+    // Bangun SQL untuk melakukan update
+    const SQL = `
+      UPDATE users
+      SET full_name = ?, institution = ?, phone = ?, birth_date = ?, gender = ?, topic = ?, skill = ?,
+          bio = ?, certification = ?, experience = ?, city = ?, time_zone = ?
+      WHERE id = ?
+    `;
+
+    // Eksekusi query update
+    const [result] = await db.query(SQL, [full_name, institution, phone, birth_date, gender, topic, skill, bio, certification, experience, city, time_zone, userId]);
+
+    if (result.affectedRows === 0) {
+      // Jika tidak ada baris yang terpengaruh, artinya pengguna dengan ID tersebut tidak ditemukan
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Ambil data pengguna yang telah diupdate
+    const [updatedUserResults] = await db.execute('SELECT * FROM users WHERE id = ?', [userId]);
+    const updatedUser = updatedUserResults[0];
+
+    // Update informasi pengguna dalam sesi
+    req.session.user = {
+      ...req.session.user,  // Gunakan data yang sudah ada dalam sesi
+      full_name: updatedUser.full_name,
+      institution: updatedUser.institution,
+      phone: updatedUser.phone,
+      birth_date: updatedUser.birth_date,
+      gender: updatedUser.gender,
+      topic: updatedUser.topic,
+      skill: updatedUser.skill,
+      bio: updatedUser.bio,
+      certification: updatedUser.certification,
+      experience: updatedUser.experience,
+      city: updatedUser.city,
+      time_zone: updatedUser.time_zone,
+    };
+
+    return res.status(200).json({ message: 'User profile updated successfully', user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error', error: error.message });
+  }
+});
+
+
+
 
 
 app.use(middlewareLogRequest);
