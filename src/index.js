700d418ad36f2ee40de8a5567a0843ee4f5f6224
putrialifiarizky <alifiarizky08@gmail.com>
2023-12-20 02:37:33 +0700

    ganti foto seperti biasa

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ app.use(
 );
 
 const corsOptions = {
-  origin: 'http://localhost:3301',
+  origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
